265205a1e661a5757306a93495f9e25320462dc7
Humberto Diógenes <hdiogenes@gmail.com>
2020-12-08 16:00:29 -0300

    Version bump to 0.0.9

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ with codecs.open('README.rst', encoding='utf-8') as f:
 
 setup(
     name='kytos_sphinx_theme',
-    version='0.0.8',
+    version='0.0.9',
     description=' Kytos Sphinx Theme',
     long_description=readme,
     install_requires=install_reqs,
